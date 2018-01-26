e71b36642ea83a4d682304323ba33788b6051777
Vladas Maldzius <2262803m@student.gla.ac.uk>
2018-01-26 09:51:33 +0000

    Tweak settings

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -19,6 +19,8 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -29,6 +31,10 @@ SECRET_KEY = '&k!j_meqp@b7x5=#(e87)l2*7cv37q8_m8zn(tw6hppe^1w_)u'
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 
 
 # Application definition
@@ -66,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 from rango import views
 
 urlpatterns = [
@@ -10,4 +12,4 @@ urlpatterns = [
     # with rango/ to be handled by
     # the rango application
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
