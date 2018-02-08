0e1323ef5bb39633cbe10206e2021c4f6bc4aceb
Vladas Maldzius <2262803m@student.gla.ac.uk>
2018-02-08 23:10:57 +0000

    Cleanup

--- rango/urls.py
+++ rango/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
 from rango import views
 
-app_name = 'rango'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
@@ -10,5 +9,5 @@ urlpatterns = [
         views.show_category, name='show_category'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$',
         views.add_page, name='add_page'),
-    url(r'^register/$', views.register, name='register'),
+    url(r'^register/$', views.register, name='register'),
 ]
